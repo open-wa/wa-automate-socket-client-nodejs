257429356abf8d7ec28ab807dbd4500961539fb3
M SHAH <shah@idk.uno>
2021-10-09 04:14:52 +0100

    🐛 fix broken enum import

--- src/MessageCollector.ts
+++ src/MessageCollector.ts
@@ -1,7 +1,95 @@
 import { EventEmitter2 } from 'eventemitter2';
-import { SimpleListener , Chat, ChatId, Message, MessageId } from '@open-wa/wa-automate-types-only';
+import type {  Chat, ChatId, Message, MessageId  } from '@open-wa/wa-automate-types-only';
 import { Collector, CollectorOptions } from './Collector';
-
+// { SimpleListener , Chat, ChatId, Message, MessageId }
+/**
+ * An enum of all the "simple listeners". A simple listener is a listener that just takes one parameter which is the callback function to handle the event.
+ */
+ export enum SimpleListener {
+  /**
+   * Represents [[onMessage]]
+   */
+  Message = 'onMessage',
+  /**
+   * Represents [[onAnyMessage]]
+   */
+  AnyMessage = 'onAnyMessage',
+  /**
+   * Represents [[onMessageDeleted]]
+   */
+  MessageDeleted = 'onMessageDeleted',
+  /**
+   * Represents [[onAck]]
+   */
+  Ack = 'onAck',
+  /**
+   * Represents [[onAddedToGroup]]
+   */
+  AddedToGroup = 'onAddedToGroup',
+  /**
+   * Represents [[onChatDeleted]]
+   */
+  ChatDeleted = 'onChatDeleted',
+  /**
+   * Represents [[onBattery]]
+   */
+  Battery = 'onBattery',
+  /**
+   * Represents [[onChatOpened]]
+   */
+  ChatOpened = 'onChatOpened',
+  /**
+   * Represents [[onIncomingCall]]
+   */
+  IncomingCall = 'onIncomingCall',
+  /**
+   * Represents [[onGlobalParticipantsChanged]]
+   */
+  GlobalParticipantsChanged = 'onGlobalParticipantsChanged',
+  /**
+   * Represents [[onChatState]]
+   */
+  ChatState = 'onChatState',
+  /**
+   * Represents [[onLogout]]
+   */
+  Logout = 'onLogout',
+  // Next two require extra params so not available to use via webhook register
+  // LiveLocation = 'onLiveLocation',
+  // ParticipantsChanged = 'onParticipantsChanged',
+  /**
+   * Represents [[onPlugged]]
+   */
+  Plugged = 'onPlugged',
+  /**
+   * Represents [[onStateChanged]]
+   */
+  StateChanged = 'onStateChanged',
+  /**
+   * Represents [[onButton]]
+   */
+  Button = 'onButton',
+  /**
+   * Requires licence
+   * Represents [[onStory]]
+   */
+  Story = 'onStory',
+  /**
+   * Requires licence
+   * Represents [[onRemovedFromGroup]]
+   */
+  RemovedFromGroup = 'onRemovedFromGroup',
+  /**
+   * Requires licence
+   * Represents [[onContactAdded]]
+   */
+  ContactAdded = 'onContactAdded',
+  /**
+   * Requires licence
+   * Represents [[onContactAdded]]
+   */
+  Order = 'onOrder',
+}
 /**
  * @typedef {CollectorOptions} MessageCollectorOptions
  * @property {number} max The maximum amount of messages to collect
