256106994d14d126060f019e82e24b454bc3a389
M SHAH <shah@idk.uno>
2022-04-23 10:16:53 +0100

    ✅  tests now include `awaitMessages`

--- test/index.js
+++ test/index.js
@@ -10,6 +10,16 @@ const test = async () => {
     console.log(`Client Connected:`, sc.socket.id)
     await sc.onAnyMessage(m=>console.log('ANY', m.body))
     await sc.sendText(TEST_NUM, 'Socket test').then(console.log)
+    try {
+        const collectedMessages = await sc.awaitMessages(TEST_NUM,m=>m.body==="test",{
+            time: 10 * 1000,
+            max: 1,
+            errors: ["time"]
+        })
+        console.log(`Collected Messages:`, collectedMessages.first())
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 test()
